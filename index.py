fc05426d61d050efd3a20345d4e4b50da1a5cb60
lichongfu <113510705+lichongfu@users.noreply.github.com>
2023-04-22 16:04:46 +0800

    Create index.py

---
+++ index.py
@@ -0,0 +1,16 @@
+import openai
+openai.api_key="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"
+print("歡迎光臨聯大算命中心")
+name=input("請輸入你的姓名: ")
+sex=input("請輸入你的性別: ")
+star=input("請輸入你的星座: ")
+prompt="假設你是一位專業的星座專家，現在有一位顧客，他叫做"+name+"，他的性別是"+sex+"，他的星座是"+star+"，請說明他這一週的運勢，如果他的性別是男。就強調運勢；如果他的性別是男，就強調愛情。"
+#print("我們設計的提示語：",prompt)
+chat_output=openai.ChatCompletion.create(
+model="gpt-3.5-turbo",
+messages=[{
+    "role":"user",
+    "content":prompt
+}])
+re_content=chat_output.choices[0].message.content
+print("一周星座運勢:", re_content)
